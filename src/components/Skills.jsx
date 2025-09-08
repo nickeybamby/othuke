368a41d990b7e45c8ae36eae0ce15615f33adecf
nicholas <nicholasobazei@gmail.com>
2025-09-08 12:55:20 +0100

    fixed error codes for the skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,7 +20,7 @@ const Skills = ({ theme }) => {
     {
       iconLight: skillLight,
       iconDark: skillDark,
-      title: "Public Speaking & Author Engagement",
+      title: "Public Speaking & Engagement",
     },
     {
       iconLight: skillLight,
@@ -35,15 +35,15 @@ const Skills = ({ theme }) => {
     {
       iconLight: skillLight,
       iconDark: skillDark,
-      title: "Distribution & Supply Chain Management",
+      title: "Supply Chain Management",
     },
   ];
 
   return (
-    <div className="relative flex flex-col items-center gap-7 px-4 sm:px-8 lg:px-16 xl:px-32 pt-30 text-gray-700 dark:text-gray-200">
+    <div className="relative flex flex-col items-center gap-5 px-4 sm:px-8 lg:px-16 xl:px-32 pt-30 text-gray-700 dark:text-gray-200">
       <Title title="Skills & Expertise" desc="From strategy to Execution" />
 
-      <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3">
+      <div className="flex flex-col items-center md:grid md:grid-cols-2 lg:grid-cols-3">
         {skillsData.map((skill, index) => (
           <SkillsCard key={index} skill={skill} index={index} theme={theme} />
         ))}
--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const SkillsCard = ({ theme, skill, index }) => {
-  return (
-    <div className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10">
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [hovering, setHovering] = useState(false);
 
+  const divRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    const bounds = divRef.current.getBoundingClientRect();
+    setPosition({ x: e.clientX - bounds.left, y: e.clientY - bounds.top });
+  };
+
+  return (
+    <div
+      className="relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10"
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
+      onMouseMove={handleMouseMove}
+      ref={divRef}
+    >
       {/* Gradient background blob */}
-      <div className="pointer-events-none rounded-full blur-2xl bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute -top-20 -left-20 z-0 transition-opacity duration-500 mix-blend-lighten opacity-70" />
+      <div
+        className={`absolute rounded-full blur-2xl bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] transition-opacity duration-500 ${
+          hovering ? "opacity-70" : "opacity-0"
+        }`}
+        style={{ top: position.y - 150, left: position.x - 150 }}
+      />
 
       {/* Card Content */}
-      <div className="flex items-center gap-6 p-4 bg-white/40 dark:bg-gray-900 relative z-10 transition-all duration-300 hover:scale-105">
-        <div className="bg-gray-100 dark:bg-gray-700 rounded-full p-2">
-          <img
-            src={theme === "dark" ? skill.iconDark : skill.iconLight}
-            alt={skill.title}
-            className="w-8 rounded-full"
-          />
-        </div>
+      <div className="relative z-10 flex items-center gap-8 p-5 hover:p-4 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-800">
         <h3 className="text-sm sm:text-base font-bold text-gray-900 dark:text-gray-100">
           {skill.title}
         </h3>
